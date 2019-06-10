03ab143970337abab17356c2c585b6e381877d04
SantosHM <dosantoshm@gmail.com>
2019-06-10 12:26:41 -0500

    first commit

---
+++ detail.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<?php include 'inc/head_common.php'; ?>
+	<title>Workshop Primero Detalle</title>
+</head>
+<body>
+	<?php include 'inc/header.php'; ?>
+	
+	<h1>detalle</h1>
+	
+	<?php include 'inc/footer.php'; ?>
+
+	<?php include 'inc/footer_common.php'; ?>
+</body>
+</html>
