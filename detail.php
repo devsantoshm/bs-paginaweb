91d0752ad17f53c0576145d29c9d94d5fc6a3b21
SantosHM <dosantoshm@gmail.com>
2019-06-11 12:32:18 -0500

    Estructura central de la maquetación detalle, header y formadores

--- detail.php
+++ detail.php
@@ -8,7 +8,62 @@
 <body>
 	<?php include 'inc/header.php'; ?>
 	
-	<h1>detalle</h1>
+	<article id="w1-detail" class="detail">
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<h2>Desarrolla front-end like a boss aprendiendo Bootstrap 3</h2>
+					</div>
+				</div>
+			</div>
+		</header>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-7">
+					<section id="intro">
+						<h3>En este workshop aprenderás...</h3>
+						<p>En este workshop de 7 horas de duración Marcos González y Armando Sotoca te mostrarán como optimizar tu flujo de trabajo a la hora de realizar el front-end de un proyecto web. La metodología apoyada en el framework Bootstrap 3 parte desde la fase de diseño hasta finalizar en la fase de desarrollo. Aprenderás a diseñar para Bootstrap, a orientar el diseño bajo la filosofía mobile first y responsive web design y descubrirás la metodología adecuada para convertir ese diseño en una maqueta HTML5 de una forma ágil y sencilla.</p>	
+						<p>Como siempre podrás complementar todo lo que aprendas durante el workshop compartiendo experiencias con otros asistentes y el/los ponentes durante la comida o la cena típica asturiana que degustaran todos los que se animen.</p>	
+					</section>
+					<section id="trainer-list">
+						<h3>Los formadores</h3>
+						<div class="trainer row">
+							<div class="col-xs-4">
+								<img src="img/marcos_profile.png" alt="Marcos González">
+							</div>
+							<div class="col-xs-8">
+								<h4>Marcos González</h4>
+								<p>
+									Fundador y director de [Q] interactiva.<br/>
+									Co-fundador  y CTO de Infocket.<br/>
+									Formador en video2brain.
+								</p>
+								<a href="http://about.me/qmarcos" class="trainer-link">CV online</a><a href="https://twitter.com/qmarcos" class="trainer-link">@qmarcos</a>
+							</div>
+						</div>
+						<div class="trainer row">
+							<div class="col-xs-4">
+								<img src="img/armando_profile.png" alt="Armando Sotoca">
+							</div>
+							<div class="col-xs-8">
+								<h4>Armando Sotoca</h4>
+								<p>
+									Diseñador todoterreno en [Q] interactiva.<br/>
+									Autor de criteriondg.info<br/>
+									Formador en video2brain.
+								</p>
+								<a href="http://www.criteriondg.info/portfolio" class="trainer-link">CV online</a><a href="https://twitter.com/skeku" class="trainer-link">@skeku</a>
+							</div>
+						</div>
+					</section>
+				</div>
+				<div class="col-xs-5">
+					
+				</div>
+			</div>
+		</div>
+	</article>
 	
 	<?php include 'inc/footer.php'; ?>
 
